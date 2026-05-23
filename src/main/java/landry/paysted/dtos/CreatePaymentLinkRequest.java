2b6c9b29719d3c0a11f97ba76559f10b237d9c5f
Landry Okoye <landryokoye373@gmail.com>
2026-05-23 14:29:22 +0100

    -feat: created payment link dto

--- src/main/java/landry/paysted/dtos/CreatePaymentLinkRequest.java
+++ src/main/java/landry/paysted/dtos/CreatePaymentLinkRequest.java
@@ -2,16 +2,29 @@ package landry.paysted.dtos;
 
 import java.util.Enumeration;
 
+import org.jspecify.annotations.NonNull;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import landry.paysted.LinkTypes;
 
 public record CreatePaymentLinkRequest(
+    @NotNull
     boolean fixed,
+    @NotNull
     boolean oneTime,
+    @NotNull
     LinkTypes LinkType,
+    @NotNull
     String name,
+    @NotNull
     String title,
+    @NotNull
     String description,
-    String targetCurrency
+    @NotNull
+    String targetCurrency,
+    @Nullable
+    String targetAmount
     
 ) {
 
